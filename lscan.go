33e488b71355113703e32b7d74ec41c28f71377a
Ryan Smith (ace hacker) <ryandotsmith@32k.io>
2012-03-25 16:26:45 -0700

    remove verbosity and protect against writing dead tuples

--- lscan.go
+++ lscan.go
@@ -1,7 +1,6 @@
 package lscan
 
 import (
-	"fmt"
 	"io"
 	"text/scanner"
 )
@@ -21,17 +20,14 @@ func Parse(r io.Reader) (kvmap map[string]interface{}) {
 
 	for tok != scanner.EOF {
 		tex := s.TokenText()
-		fmt.Printf("handle=%v ", tex)
 		if tex == `=` {
-			fmt.Printf("split ")
 			ink = false
 			inv = true
 			tok = s.Scan()
 			continue
 		}
 		if tex == ` ` {
-			fmt.Printf("next ")
-			kvmap[key] = val
+			writeTup(kvmap, key, val)
 			key = ""
 			val = ""
 			ink = true
@@ -41,18 +37,22 @@ func Parse(r io.Reader) (kvmap map[string]interface{}) {
 		}
 		if ink {
 			key += tex
-			fmt.Printf("key=%v ", key)
 		} else if inv {
 			val += tex
-			fmt.Printf("val=%v ", val)
 		}
 
 		tok = s.Scan()
 		// We are done. :( Let us write whatever we have left
 		// to the map
 		if tok == scanner.EOF {
-			kvmap[key] = val
+			writeTup(kvmap, key, val)
 		}
 	}
 	return
+}
+
+func writeTup(m map[string]interface{}, k string, v string) {
+	if len(k) > 0 && len(v) > 0 {
+		m[k] = v
+	}
 }
--- lscan_test.go
+++ lscan_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 var str = `hello=world name="ryan\"smith" distance=1.123 desc="hi=there" time="2012-03-21 10:18:20 -0700"`
+var aStr = ` [590-1]  [PINK] LOG:  checkpoint complete: wrote 0 buffers (0.0%); 0 transaction log file(s) added, 0 removed, 1 recycled; write=0.001 s, sync=0.000 s, total=0.008 s; sync files=0, longest=0.000 s, average=0.000 s`
 
 func TestParseSimple(t *testing.T) {
 	in := strings.NewReader(str)
@@ -59,5 +60,20 @@ func TestParseEqchr(t *testing.T) {
 	expected := `"hi=there"`
 	if actual != expected {
 		t.Errorf("\n e(%v) \n a(%v)", expected, actual)
+	}
+}
+
+func TestParseAStr(t *testing.T) {
+	in := strings.NewReader(aStr)
+	m := Parse(in)
+
+	actual := m["write"]
+	expected := `0.001`
+	if actual != expected {
+		t.Errorf("\n e(%v) \n a(%v)", expected, actual)
+	}
+
+	if len(m) != 6 {
+		t.Errorf("\n expected 6 pairs \n received %v", len(m))
 	}
 }
